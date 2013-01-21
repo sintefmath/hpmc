47123fe6a72b6f3781201e66b86e61b23fb87e2f
Chris <cdyk@server.fake>
2013-01-21 09:23:32 +0100

    Added author to copyright headers

--- apps/shaders/particles_shape_fs_150.glsl
+++ apps/shaders/particles_shape_fs_150.glsl
@@ -3,6 +3,8 @@
  *
  * This file is part of the HPMC Library.
  *
+ * Author(s): Christopher Dyken, <christopher.dyken@sintef.no>
+ *
  * HPMC is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
--- hpmc/shaders/baselevel_traversal_110.glsl
+++ hpmc/shaders/baselevel_traversal_110.glsl
@@ -1,3 +1,21 @@
+/* Copyright STIFTELSEN SINTEF 2012
+ *
+ * This file is part of the HPMC Library.
+ *
+ * Author(s): Christopher Dyken, <christopher.dyken@sintef.no>
+ *
+ * HPMC is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * HPMC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * HPMC.  If not, see <http://www.gnu.org/licenses/>.
+ */
 //          Scale tp from tile parameterization to scalar field parameterization
 //          Now we have found the MC cell, next find which edge that this vertex lies on
 // Output sample positions, if needed.
